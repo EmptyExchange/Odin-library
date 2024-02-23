b1f88fea0838f2f071e3d9be98f5e20446854dc8
EmptyExchange <Michael.young07@outlook.com>
2024-02-23 16:35:10 -0600

    added script and style for form, not working though

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function Book(title, author, pages, read) {
     }
 }
 
+function addBookToLibrary(){
+
+}
+//This section for dialog box
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => { dialog.showModal();
+});
+
+//"Close" button closes the dialog
+closeButton.addEventListener("click", () => { dialog.close();
+});
