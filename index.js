c02bbb05755325bfa9a06616cf688c9f4f838dd4
EmptyExchange <Michael.young07@outlook.com>
2024-02-27 11:48:19 -0600

    still cant get it to work

--- index.js
+++ index.js
@@ -10,18 +10,16 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary(){
-
-}
-//This section for dialog box
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
 
 // "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => { dialog.showModal();
+showButton.addEventListener("click", () => {
+  dialog.showModal();
 });
 
-//"Close" button closes the dialog
-closeButton.addEventListener("click", () => { dialog.close();
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
 });
